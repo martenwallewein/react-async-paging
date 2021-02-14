1479113fee33e5e46f6ffc9b1333c1641677b7bb
Marten Gartner <martenwallewein@web.de>
2021-02-14 07:40:09 +0100

    Fix vite build isses

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import { AsyncPaging } from "./components/AsyncPaging";
+export * from "./components/AsyncPaging";
 export * from "./types/AsyncPaging";
 export * from "./types/FetchData";
-
-export {
-    AsyncPaging
-}
